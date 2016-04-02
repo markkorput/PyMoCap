993c21b81ce852551634af184a70ff0df427b4d7
Mark van de Korput <dr.theman@gmail.com>
2016-04-02 12:05:51 +0200

    FpsSync in own file

--- pymocap/readers/natnet_file_reader.py
+++ pymocap/readers/natnet_file_reader.py
@@ -2,42 +2,7 @@ from pymocap.color_terminal import ColorTerminal
 from pymocap.manager import Manager
 from pymocap.event import Event
 from pymocap.natnet_file import NatnetFile
-
-from datetime import datetime
-
-class FpsSync:
-    def __init__(self, fps=None):
-        self.fps = fps
-
-        if not self.fps:
-            self.fps = 120.0
-
-        self._dtFrame = 1.0/self.fps
-        self.reset()
-
-    def start(self):
-        self.reset()
-
-    def reset(self):
-        self.startTime = datetime.now()
-        self.frameCount = 0
-        self._nextFrameTime = 0
-
-    def time(self):
-        return (datetime.now()-self.startTime).total_seconds()
-
-    def timeForNewFrame(self):
-        return self.time() >= self._nextFrameTime
-
-    def doFrame(self):
-        self._nextFrameTime += self._dtFrame
-
-    def nextFrame(self):
-        if not self.timeForNewFrame():
-            return False
-
-        self.doFrame()
-        return True
+from pymocap.fps_sync import FpsSync
 
 class NatnetFileReader:
     def __init__(self, options = {}):
@@ -48,7 +13,6 @@ class NatnetFileReader:
         # events
         self.startEvent = Event()
         self.stopEvent = Event()
-        self.updateEvent = Event()
 
         # autoStart is True by default
         if not 'autoStart' in options or options['autoStart']:
